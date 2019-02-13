bba22a55382828e2c9c62dda483b3ead83a537aa
Rick-97 <33564174+HenriqueChaves97@users.noreply.github.com>
2019-02-13 19:07:40 -0300

    Add files via upload

---
+++ Search_min_max.py
@@ -0,0 +1,22 @@
+x = [input(f"Numero {i+1}: \n") for i in range(0, 8)]
+
+def test(operation):
+    cont = 1
+    valor = ""
+    for k in x:
+        for j in x:
+            if x.index(k) == x.index(j):
+                pass
+            else:
+                if valor != k:
+                    cont = 0
+                if eval(f" {k} {operation} {j}"):
+                    valor = k
+                    cont +=1
+                    if cont == (len(x)-1):
+                        return k
+                else:
+                    pass
+                
+print(f" Maior: {test('>')}")
+print(f" Menor: {test('<')}")
